99c7d64828cd30856d2868e926fd088e100e4508
sreurosign-saurav <sreurosign@gmail.com>
2026-01-17 14:55:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import requests
-import base64
 from groq import Groq
+import requests, base64, os
 
 app = FastAPI()
 
@@ -14,11 +13,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-client = Groq(api_key="PASTE_GROQ_API_KEY_HERE")
+# ENV KEYS
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+HF_API_KEY = os.getenv("HF_API_KEY")
 
-HF_API_KEY = "PASTE_HUGGINGFACE_API_KEY_HERE"
+client = Groq(api_key=GROQ_API_KEY)
+
 SD_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2"
-
 headers = {
     "Authorization": f"Bearer {HF_API_KEY}"
 }
@@ -30,16 +31,25 @@ class ImageRequest(BaseModel):
     prompt: str
 
 
+@app.get("/")
+def root():
+    return {"status": "server running"}
+
+
 @app.post("/chat")
 def chat(req: ChatRequest):
-    completion = client.chat.completions.create(
-        model="llama3-8b-8192",
-        messages=[
-            {"role": "system", "content": "You are Amraa AI assistant."},
-            {"role": "user", "content": req.message}
-        ]
-    )
-    return {"reply": completion.choices[0].message.content}
+    try:
+        completion = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=[
+                {"role": "system", "content": "You are Amraa AI assistant."},
+                {"role": "user", "content": req.message}
+            ]
+        )
+        return {"reply": completion.choices[0].message.content}
+
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.post("/image")
@@ -49,6 +59,9 @@ def generate_image(req: ImageRequest):
         headers=headers,
         json={"inputs": req.prompt}
     )
+
+    if response.status_code != 200:
+        return {"error": "Image generation failed"}
 
     image_base64 = base64.b64encode(response.content).decode("utf-8")
     return {"image": image_base64}
